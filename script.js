1b98bfa09bd4e5e5eb2f89f60a4beed79051ff7b
Michael Lutz <mikelutz2400@gmail.com>
2023-05-11 03:06:27 -0400

    flower, leaf, moon

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-import { digits, chars } from "./symbols.js";
+import { digits, chars, symbols } from "./symbols.js";
 const gridSize = 64;
 const middleX = gridSize / 2;
 const middleY = gridSize / 4;
 let eraserActive = false;
 let mouseDown = false;
 let currColor = "#000000";
-let currHour = 0;
-let currMin = 0;
+let currHour = "0";
+let currMin = "0";
 let isTime = false;
 const apiKey = "27447ca21d92d7fec85b3deadb996969";
 
@@ -25,16 +25,7 @@ time.addEventListener("click", (e) => {
   if (isTime) {
     drawTime();
   } else {
-    let x = middleX - 10;
-    let y = middleY - 5;
-    clearSpace(
-      x,
-      y,
-      getWidthDigit(0) * (getNumDigits(currHour) + 2) +
-        getWidthDigit(14) +
-        getNumDigits(currHour),
-      getHeightDigit(0)
-    );
+    clearString(`${currHour}:${currMin}`, middleX, middleY);
   }
 });
 clear.addEventListener("click", resetGrid);
@@ -106,132 +97,112 @@ function changeColor(e) {
 function start() {
   generateGrid(gridSize);
 }
-
+const leafColors = ["#CC5200", "#F2A069"];
+const moonColors = ["#32302F", "#BABABA", "#777787"];
+const flowerColors = [
+  "#FEAE35",
+  "#FEE861",
+  "#D77643",
+  "#BE4B30",
+  "#3E8948",
+  "#265C42",
+  "#F67622",
+  "#3A4467",
+  "#5A6988",
+  "#8B9BB4",
+];
 window.onpageshow = () => {
   start();
+  drawSymbol("flower", 0, 0, flowerColors);
 };
 
-function drawLetter(num, x, y, color) {
-  let char = chars[num];
-  for (let i = 0; i < char.length; i++) {
-    for (let j = 0; j < char[0].length; j++) {
-      if (char[i][j] == 1) {
-        grid.children[j + x].children[i + y].style.backgroundColor = `${color}`;
+function drawSymbol(symbol, x, y, ...color) {
+  const sym = symbols[symbol];
+  for (let i = 0; i < sym.length; i++) {
+    for (let j = 0; j < sym[0].length; j++) {
+      if (sym[i][j] == 1) {
+        console.log(color[0][0]);
+        grid.children[j + x].children[i + y].style.backgroundColor = color[0][0];
+      } else if (sym[i][j] == 2) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][1]}`;
+      } else if (sym[i][j] == 3) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][2]}`;
+      } else if (sym[i][j] == 4) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][3]}`;
+      } else if (sym[i][j] == 5) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][4]}`;
+      } else if (sym[i][j] == 6) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][5]}`;
+      } else if (sym[i][j] == 7) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][6]}`;
+      } else if (sym[i][j] == 8) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][7]}`;
+      } else if (sym[i][j] == 9) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][8]}`;
+      } else if (sym[i][j] == 10) {
+        grid.children[j + x].children[i + y].style.backgroundColor = `${color[0][9]}`;
       } else {
         grid.children[j + x].children[i + y].style.backgroundColor = "#f1f1f1";
       }
     }
   }
 }
-function drawDigit(num, x, y, color) {
-  let number = digits[num];
-  for (let i = 0; i < number.length; i++) {
-    for (let j = 0; j < number[0].length; j++) {
-      if (number[i][j] == 1) {
-        grid.children[j + x].children[i + y].style.backgroundColor = `${color}`;
-      } else {
-        grid.children[j + x].children[i + y].style.backgroundColor = "#f1f1f1";
-      }
-    }
+function drawString(str, x, y, color) {
+  for (let i = 0; i < str.length; i++) {
+    drawSymbol(str[i], x, y, [color]);
+    x += getWidth(str[i]) + 1;
   }
 }
-function clearSpace(x, y, width, height) {
-  for (let i = 0; i < height; i++) {
-    for (let j = 0; j < width; j++) {
+function clearString(str, x, y) {
+  for (let i = 0; i < str.length; i++) {
+    clearSymbol(str[i], x, y);
+    x += getWidth(str[i]) + 1;
+  }
+}
+function clearSymbol(symbol, x, y) {
+  const sym = symbols[symbol];
+  for (let i = 0; i < sym.length; i++) {
+    for (let j = 0; j < sym[0].length; j++) {
       grid.children[j + x].children[i + y].style.backgroundColor = "#f1f1f1";
     }
   }
 }
-function drawNumber(num, x, y) {
-  let offset = 0;
-  if (num < 0) {
-    offset = 6;
-    num = Math.abs(num);
-    drawDigit(10, x, y, "black");
-  }
-  let numString = num.toString();
-  for (let h = 0; h < numString.length; h += 1) {
-    drawDigit(numString[h], x + offset, y, "black");
-    offset += 6;
-  }
-}
-function getNumDigits(num) {
-  let tot = 0;
-  while (num > 0) {
-    num = Math.floor(num / 10);
-    tot++;
-  }
-  return tot;
-}
-function drawTemp(num, x, y) {
-  let offset = 0;
-  if (num < 0) {
-    offset = 6;
-    num = Math.abs(num);
-    drawDigit(10, x, y, "black");
-  }
-  let numString = num.toString();
-  for (let h = 0; h < numString.length; h += 1) {
-    drawDigit(numString[h], x + offset, y, "black");
-    offset += 6;
-  }
-  drawDigit(11, x + offset, y, "black");
-}
 
+function getHeight(key) {
+  const sym = symbols[key];
+  return sym.length;
+}
+function getWidth(key) {
+  const sym = symbols[key];
+  return sym[0].length;
+}
 async function getData(city) {
   const data = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
   );
   const weather = await data.json();
   let tempKelvin = Math.floor(weather.main.temp);
-  let temp = Math.floor(((tempKelvin - 273.15) * 9) / 5 + 32);
-  drawDigit(13, 10, 7, "orange");
-  drawTemp(temp, gridSize / 2 + 1, gridSize / 4 - 3, "black");
-}
-function getWidthDigit(num) {
-  return digits[num][0].length;
-}
-function getWidthChar(num) {
-  return chars[num][0].length;
-}
-function getHeightDigit(num) {
-  return digits[num].length;
-}
-function getHeightChar(num) {
-  return chars[num].length;
+  let temp = Math.floor(((tempKelvin - 273.15) * 9) / 5 + 32).toString();
+  drawSymbol("sun", 10, 7, ["orange"]);
+  drawString(temp + "*", gridSize / 2 + 1, gridSize / 4 - 3, "black");
 }
 function drawTime() {
   if (isTime) {
-    let date = new Date();
-    let x = middleX - 10;
-    let y = middleY - 5;
-    currHour = date.getHours();
-    currMin = date.getMinutes();
-    clearSpace(
-      x,
-      y,
-      getWidthDigit(0) * (getNumDigits(currHour) + 2) +
-        getWidthDigit(14) +
-        getNumDigits(currHour),
-      getHeightDigit(0)
-    );
-    let offset;
-    if (currHour > 12) {
-      offset = getNumDigits(currHour) * getWidthDigit(0) + 1;
-      currHour -= 12;
-    } else if (currHour == 0) {
-      currHour = 12;
-    } else {
-      offset = getNumDigits(currHour) * getWidthDigit(0);
-    }
-    drawNumber(currHour, x, y, "black");
-    drawDigit(14, x + offset, y, "black");
-    if (currMin < 10) {
-      drawNumber(0, x + offset + 3, y, "black");
-      drawNumber(currMin, x + offset + 9, y, "black");
-    } else {
-      drawNumber(currMin, x + offset + 3, y, "black");
-    }
+    getTime();
+    drawString(`${currHour}:${currMin}`, middleX, middleY, "black");
   }
+}
+function getTime() {
+  let date = new Date();
+  currHour = date.getHours();
+  currMin = date.getMinutes();
+  if (currHour > 12) {
+    currHour -= 12;
+  }
+  if (currMin < 10) {
+    currMin = "0" + currMin.toString();
+  }
+  currHour = currHour.toString();
+  currMin = currMin.toString();
 }
 setInterval(drawTime, 1000);
